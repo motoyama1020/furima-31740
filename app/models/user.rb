83ce27874efffb38a7d60f7dd41800d7ec3df03d
motoyama1020 <babywalk18@gmail.com>
2020-11-10 21:52:10 +0900

    単体テストコードの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,16 +5,16 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates_format_of :password, with: PASSWORD_REGEX
   validates :nickname, presence: true
   validates :birthday, presence: true
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
     validates :first_name
     validates :last_name
   end
   
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: '全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
     validates :first_name_kana
     validates :last_name_kana
   end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname   {Faker::Name}
-    email      {Faker::Internet.free_emamil}
-    password   {Faker::Internet.password((min_length: 6))}
+    nickname              {Faker::Name.initials}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6, mix_case: true)}
     password_confirmation {password}
-    first_name {Faker::Name.first_name}
-    last_name {Faker::Name.last_name}
-    first_name_kana
-    last_name_kana 
-    birthday   {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
+    first_name            {Faker::Name.first_name}
+    last_name             {Faker::Name.last_name}
+    first_name_kana       {"タロウ"}
+    last_name_kana        {"ヤマダ"}
+    birthday              {Faker::Date.between(from: '1930-01-01', to: '2015-12-31')}
   end
 end
