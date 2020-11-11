da1789fa312bec69972cae65328b7714038f05b6
motoyama1020 <babywalk18@gmail.com>
2020-11-11 15:47:59 +0900

    activehashモデルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.includes(:user)
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :status_id, :burden_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -13,4 +13,5 @@ class Item < ApplicationRecord
   
   belongs_to :user
   has_one :buy
+  has_one_attached :image
 end
