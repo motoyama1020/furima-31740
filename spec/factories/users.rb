c25d6cb2f95a443595792dd9259529d2ca2d954e
motoyama1020 <babywalk18@gmail.com>
2020-11-12 16:34:30 +0900

    javascript導入、手数料＆利益入力

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :user do
     nickname              { Faker::Name.initials }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6, mix_case: true) }
+    password              { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { Faker::Name.first_name }
     last_name             { Faker::Name.last_name }
