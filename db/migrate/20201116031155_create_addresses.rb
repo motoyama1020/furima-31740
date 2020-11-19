c0240427a96d7d458b28427dbd79cec6dd3b7a3e
motoyama1020 <babywalk18@gmail.com>
2020-11-19 12:39:27 +0900

    addressマイグレーションファイルの制約追加

--- db/migrate/20201116031155_create_addresses.rb
+++ db/migrate/20201116031155_create_addresses.rb
@@ -3,11 +3,11 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
     create_table :addresses do |t|
       t.string :post_num,       default: "",  null: false
       t.integer :prefecture_id,               null: false
-      t.string :city,           default: ""
-      t.string :house_num,      default: ""
+      t.string :city,           default: "",  null: false
+      t.string :house_num,      default: "",  null: false
       t.string :building,       default: ""
-      t.string :phone,          default: ""
-      t.references :buy,                     null: false,  foreign_key: true
+      t.string :phone,          default: "",  null: false
+      t.references :buy,                      null: false,  foreign_key: true
       t.timestamps
     end
   end
